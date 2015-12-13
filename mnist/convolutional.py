27eee22389bdd4ab959621b8a5e03709ba564c21
masaponto <masaponto@gmail.com>
2015-12-13 09:27:10 +0900

    [ADD] add data_adder but not work

--- mnist/convolutional.py
+++ mnist/convolutional.py
@@ -5,7 +5,8 @@
 import tensorflow as tf
 
 import input_data
-data = input_data.read_data_sets("/tmp/data/", one_hot=True)
+#data = input_data.read_data_sets("/tmp/data/", one_hot=True)
+data = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # model
 import model
@@ -18,7 +19,7 @@ with tf.variable_scope("convolutional"):
 y_ = tf.placeholder("float", [None, 10])
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 saver = tf.train.Saver(variables)
@@ -28,11 +29,14 @@ with tf.Session() as sess:
     for i in range(20000):
         batch = data.train.next_batch(50)
         if i % 100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-            print "step %d, training accuracy %g"%(i, train_accuracy)
-        sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+            train_accuracy = accuracy.eval(
+                feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print "step %d, training accuracy %g" % (i, train_accuracy)
+        sess.run(train_step, feed_dict={
+                 x: batch[0], y_: batch[1], keep_prob: 0.5})
 
     print sess.run(accuracy, feed_dict={x: data.test.images, y_: data.test.labels, keep_prob: 1.0})
 
-    path = saver.save(sess, "data/convolutional.ckpt")
+    #path = saver.save(sess, "data/convolutional.ckpt")
+    path = saver.save(sess, "data/convolutional_tmp.ckpt")
     print "Saved:", path
